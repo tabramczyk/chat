255c49f8de3a76d03ebfc584526457232462d424
tabramczyk <Tomasz.Abramczyk@fis.agh.edu.pl>
2019-09-18 15:13:30 +0200

    Updates

---
+++ precache-manifest.8d548eb74bbf85e4d2432ebb950fa03e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af68408c8f26aa90eb28a407978ea57f",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "0582df6e15dbe08b7465",
+    "url": "/chat/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "17e8f4d22ca7f94329d0",
+    "url": "/chat/static/js/2.a61ba000.chunk.js"
+  },
+  {
+    "revision": "0582df6e15dbe08b7465",
+    "url": "/chat/static/js/main.dd1e0112.chunk.js"
+  },
+  {
+    "revision": "5bb637843dac5d879ad5",
+    "url": "/chat/static/js/runtime~main.54cd968c.js"
+  }
+]);
