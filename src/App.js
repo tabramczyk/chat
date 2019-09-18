0e53b08330a4493271a7324023973cb4f7ecbec5
tabramczyk <Tomasz.Abramczyk@fis.agh.edu.pl>
2019-09-18 15:22:46 +0200

    Changing BaseRouter to HashRouter.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {HashRouter as Router, Route} from 'react-router-dom';
 import {ThemeProvider} from '@material-ui/styles';
 import CssBaseline from "@material-ui/core/CssBaseline";
 
@@ -12,7 +12,7 @@ function App() {
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
-            <Router>
+            <Router basename="/">
                 <Route exact path={ROUTES.HOME_PAGE}/>
                 <Route path={ROUTES.LOGIN_PAGE} component={LoginComponent}/>
                 <Route path={ROUTES.REGISTER_PAGE} component={RegisterComponent}/>
